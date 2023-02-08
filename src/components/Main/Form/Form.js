737b6bb9740e3f76a5596aaec430caf97c8fac75
= <=>
2023-02-08 15:53:26 -0800

    first commit

---
+++ src/components/Main/Form/Form.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { TextField, Typography, Button, MenuItem, Select, InputLabel, Grid, FormControl} from '@material-ui/core'
+import useStyles from "./styles"
+
+
+const Form = () => {
+    const classes = useStyles();
+  return (
+    <Grid container spacing = {2}>
+        <Grid item xs = {12}>
+            <Typography 
+            align = "center"
+            variant = "subtitle2"
+            gutterBottom>
+                ...
+            </Typography>
+        </Grid>
+        <Grid item xs = {6}>
+            <FormControl fullWidth>
+                <InputLabel>Type</InputLabel>
+                <Select>
+                    <MenuItem value = "Income">Income</MenuItem>
+                    <MenuItem value = "Income">Expense</MenuItem>
+                </Select>
+            </FormControl>
+        </Grid>
+        <Grid item xs= {6}>
+            <FormControl fullWidth>
+                <InputLabel>Category</InputLabel>
+                <Select>
+                    <MenuItem value = "business">Business</MenuItem>
+                     <MenuItem value = "salary">Salary</MenuItem>
+                </Select>
+            </FormControl>
+        </Grid>
+        <Grid item xs = {6}>
+            <TextField type = "number" label = "Amount" fullWidth/>
+        </Grid>
+         <Grid item xs = {6}>
+            <TextField type = "date" label = "date" fullWidth/>
+        </Grid>
+        <Button className = {classes.button} variant = "outlined" color = "primary" fullwidth>Create</Button>
+    </Grid>
+  )
+}
+
+export default Form
